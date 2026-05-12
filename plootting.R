7a7bd463d997e754692676ce0aedb87933736c0a
HannahMTucker <hannahhmt27@gmail.com>
2026-05-12 15:08:43 -0700

    wompwomwpwomp

--- plootting.R
+++ plootting.R
@@ -1,1 +1,8 @@
-yayayayayay
+library(ggplot2)
+library(tidyverse)
+
+respiration_data <- read.csv("data/datasheet - RESPIRATION.csv") %>%
+  mutate()
+
+ggplot(data=respiration_data) %>%
+  geom_boxplot(aes(x=))
